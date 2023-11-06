9252be0401ec7a76a25bd1094a928ae3ddf129a6
Yordan Bratanov <jordan.bratanov@yahoo.com>
2023-11-06 09:27:18 +0200

    creating domain models

---
+++ Social/Social.Domain/Aggregates/PostAggregate/PostComment.cs
@@ -0,0 +1,36 @@
+﻿namespace Social.Domain.Aggregates.PostAggregate
+{
+    public class PostComment
+    {
+        private PostComment()
+        {
+
+        }
+        public Guid CommentId { get; private set; }
+        public Guid PostId { get; private set; }
+        public string Text { get; private set; }
+        public Guid UserProfileId { get; private set; }
+        public DateTime DateCreated { get; private set; }
+        public DateTime LastModified { get; private set; }
+
+        //Factories
+        public static PostComment CreatePostComment(Guid postId, string text, Guid userProfileId)
+        {
+            return new PostComment
+            {
+                PostId = postId,
+                Text = text,
+                UserProfileId = userProfileId,
+                DateCreated = DateTime.UtcNow,
+                LastModified = DateTime.UtcNow
+            };
+        }
+
+        //public methods
+        public void UpdateCommentText(string newText)
+        {
+            Text = newText;
+            LastModified = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Social/Social.Domain/Aggregates/PostAggregate/PostInteraction.cs
@@ -0,0 +1,23 @@
+﻿namespace Social.Domain.Aggregates.PostAggregate
+{
+    public class PostInteraction
+    {
+        private PostInteraction()
+        {
+
+        }
+        public Guid InteractionId { get; private set; }
+        public Guid PostId { get; private set; }
+        public InteractionType InteractionType { get; private set; }
+
+        //Factories
+        public static PostInteraction CreatePostInteraction(Guid postId, InteractionType type)
+        {
+            return new PostInteraction
+            {
+                PostId = postId,
+                InteractionType = type
+            };
+        }
+    }
+}
